da29bcbbef8f9e1ec249945d73f7e794c8117c6e
Clarks1223 <pablo.uchuari@epn.edu.ec>
2023-02-14 01:49:05 -0500

    cambios en main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         buscar venbuscar= new buscar();
         venbuscar.mostrarVentana();
-        //Formulario ven1= new Formulario();
-        //ven1.MostarVentana();
+        Formulario ven1= new Formulario();
+        ven1.MostarVentana();
     }
 }
